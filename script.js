dd212149b50825ac3633f25fd33919db80670501
syamjith <syamjithloq@gmail.com>
2026-02-13 11:42:58 +0530

    Updated design

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-// Simple welcome message
 console.log("Welcome to Panniyoor Sree Varahamoorthy Temple Website");
-// Smooth scroll (future ready)
+
+let currentLang = "ml";
+const toggleBtn = document.getElementById("langToggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.querySelectorAll("[data-ml]").forEach(el => {
+        el.innerHTML =
+            currentLang === "ml"
+                ? el.getAttribute("data-en")
+                : el.getAttribute("data-ml");
+    });
+
+    toggleBtn.textContent = currentLang === "ml" ? "ML" : "EN";
+    currentLang = currentLang === "ml" ? "en" : "ml";
+});
+
+/* Smooth scroll */
 document.querySelectorAll("a[href^='#']").forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector(link.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        document
+            .querySelector(link.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
 });
